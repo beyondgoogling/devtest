f0a07e043934f0f3e67b7a87695f2c9cf8b87c4d
David O <34683034+beyondgoogling@users.noreply.github.com>
2022-06-29 16:11:38 +0100

    Update example.js

--- example.js
+++ example.js
@@ -47,17 +47,18 @@ function init() {
   const providerOptions = {
     walletconnect: {
       package: WalletConnectProvider,
-      //options: {
-      rpc:{
-           56: "https://bsc-dataseed.binance.org/",
-           137: "https://polygon-rpc.com",
-           //100: "https://dai.poa.network",
+      options: {
+       rpc:{
+        56: "https://bsc-dataseed.binance.org/",
+       137: "https://polygon-rpc.com",
+            //100: "https://dai.poa.network",
        
        
        
         // Mikko's test key - don't copy as your mileage may vary
         //infuraId: "8043bb2cf99347b1bfadfb233c5325c0",
-      }
+      },
+    }
     },
 
     fortmatic: {
