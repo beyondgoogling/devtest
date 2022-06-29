3ccff46504a62d26a9242ca7a1e68a122edc2c01
David O <34683034+beyondgoogling@users.noreply.github.com>
2022-06-29 02:50:41 +0100

    Update example.js

--- example.js
+++ example.js
@@ -72,91 +72,7 @@ function init() {
 }
 
 
-/**
- * Kick in the UI action after Web3modal dialog has chosen a provider
- */
-// async function fetchAccountData() {
 
-//   // Get a Web3 instance for the wallet
-//   const web3 = new Web3(provider);
-
-//   console.log("Web3 instance is", web3);
-
-//   // Get connected chain id from Ethereum node
-//   const chainId = await web3.eth.getChainId();
-//   // Load chain information over an HTTP API
-//   const chainData = evmChains.getChain(chainId);
-//   document.querySelector("#network-name").textContent = chainData.name;
-
-//   // Get list of accounts of the connected wallet
-//   const accounts = await web3.eth.getAccounts();
-
-//   // MetaMask does not give you all accounts, only the selected account
-//   console.log("Got accounts", accounts);
-//   selectedAccount = accounts[0];
-
-//   document.querySelector("#selected-account").textContent = selectedAccount;
-
-//   // Get a handl
-//   const template = document.querySelector("#template-balance");
-//   const accountContainer = document.querySelector("#accounts");
-
-//   // Purge UI elements any previously loaded accounts
-//   accountContainer.innerHTML = '';
-
-//   // Go through all accounts and get their ETH balance
-//   const rowResolvers = accounts.map(async (address) => {
-//     const balance = await web3.eth.getBalance(address);
-//     // ethBalance is a BigNumber instance
-//     // https://github.com/indutny/bn.js/
-//     const ethBalance = web3.utils.fromWei(balance, "ether");
-//     const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
-//     // Fill in the templated row and put in the document
-//     const clone = template.content.cloneNode(true);
-//     clone.querySelector(".address").textContent = address;
-//     clone.querySelector(".balance").textContent = humanFriendlyBalance;
-//     accountContainer.appendChild(clone);
-//   });
-
-//   // Because rendering account does its own RPC commucation
-//   // with Ethereum node, we do not want to display any results
-//   // until data for all accounts is loaded
-//   await Promise.all(rowResolvers);
-
-//   // Display fully loaded UI for wallet data
-//   document.querySelector("#prepare").style.display = "none";
-//   document.querySelector("#connected").style.display = "block";
-// }
-
-
-
-/**
- * Fetch account data for UI when
- * - User switches accounts in wallet
- * - User switches networks in wallet
- * - User connects wallet initially
- */
-// async function refreshAccountData() {
-
-//   // If any current data is displayed when
-//   // the user is switching acounts in the wallet
-//   // immediate hide this data
-//   document.querySelector("#connected").style.display = "none";
-//   document.querySelector("#prepare").style.display = "block";
-
-//   // Disable button while UI is loading.
-//   // fetchAccountData() will take a while as it communicates
-//   // with Ethereum node via JSON-RPC and loads chain data
-//   // over an API call.
-//   document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
-//   await fetchAccountData(provider);
-//   document.querySelector("#btn-connect").removeAttribute("disabled")
-// }
-
-
-/**
- * Connect wallet button pressed.
- */
 async function onConnect() {
 
   console.log("Opening a dialog", web3Modal);
@@ -167,27 +83,7 @@ async function onConnect() {
     return;
   }
 
-  // Subscribe to accounts change
-//   provider.on("accountsChanged", (accounts) => {
-//     fetchAccountData();
-//   });
-
-//   // Subscribe to chainId change
-//   provider.on("chainChanged", (chainId) => {
-//     fetchAccountData();
-//   });
-
-//   // Subscribe to networkId change
-//   provider.on("networkChanged", (networkId) => {
-//     fetchAccountData();
-//   });
-
-//   await refreshAccountData();
-// }
-
-/**
- * Disconnect wallet button pressed.
- */
+  
 async function onDisconnect() {
 
   console.log("Killing the wallet connection", provider);
