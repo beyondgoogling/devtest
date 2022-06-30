45b5df74f3bd5129e78e015702afa35839dd22da
David O <34683034+beyondgoogling@users.noreply.github.com>
2022-07-01 00:12:17 +0100

    Update example.js

--- example.js
+++ example.js
@@ -206,7 +206,7 @@ async function onConnect() {
     console.log("Could not get a wallet connection",e,game);
     return;
   }
- if (game ===false){
+ if (game ===true){
   console.log("we are in",game);
   document.getElementById("speedbtn").style.display = "";
   document.getElementById("jmpbtn").style.display = "";
